77aba3758866adc7ed855bdee1ffcebd2bb81385
SteinerDiamine <shubhamsharma68663@gmail.com>
2025-03-20 19:34:49 +0530

    commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='font-bold text-indigo-600'>
+      hi
+    </div>
+  )
+}
+
+export default page
--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <h1 className='bg-slate-700'>App</h1>
-  )
-}
-
-export default App
